caf9b0156421acafdd08b360a0f1ed6935d515ae
Sam Colby <samcolby@users.noreply.github.com>
2017-05-11 20:43:40 +0100

    Added a few more tests

--- __tests__/DateFns.js
+++ __tests__/DateFns.js
@@ -2,11 +2,6 @@ import moment from "moment";
 
 import DateFns from "../src/DateFns";
 
-const doGetStartOfWeekTest = (input, isoStartDay, output) => {
-  const result = DateFns.getStartOfWeek(moment(input), isoStartDay);
-  expect(result.format("YYYY-MM-DD")).toBe(output);
-};
-
 const getStartOfWeekTestData = [
   {
     input: "2017-05-10",
@@ -27,13 +22,77 @@ const getStartOfWeekTestData = [
     input: "2017-05-08",
     isoStartDay: 3,
     output: "2017-05-03"
+  },
+  {
+    input: "2017-05-11",
+    isoStartDay: 4,
+    output: "2017-05-11"
+  },
+  {
+    input: "2017-05-12",
+    isoStartDay: 4,
+    output: "2017-05-11"
+  },
+  {
+    input: "2017-05-08",
+    isoStartDay: 4,
+    output: "2017-05-04"
+  },
+  {
+    input: "2017-05-12",
+    isoStartDay: 5,
+    output: "2017-05-12"
+  },
+  {
+    input: "2017-05-13",
+    isoStartDay: 5,
+    output: "2017-05-12"
+  },
+  {
+    input: "2017-05-08",
+    isoStartDay: 5,
+    output: "2017-05-05"
+  },
+  {
+    input: "2017-05-13",
+    isoStartDay: 6,
+    output: "2017-05-13"
+  },
+  {
+    input: "2017-05-14",
+    isoStartDay: 6,
+    output: "2017-05-13"
+  },
+  {
+    input: "2017-05-08",
+    isoStartDay: 6,
+    output: "2017-05-06"
+  },
+  {
+    input: "2017-05-14",
+    isoStartDay: 7,
+    output: "2017-05-14"
+  },
+  {
+    input: "2017-05-15",
+    isoStartDay: 7,
+    output: "2017-05-14"
+  },
+  {
+    input: "2017-05-08",
+    isoStartDay: 7,
+    output: "2017-05-07"
   }
 ];
 
 describe("DateFns getStartOfWeek", () => {
   getStartOfWeekTestData.forEach(item => {
     it(`should return ${item.output} when given ${item.input} and iosWeekday is ${item.isoStartDay}`, () => {
-      doGetStartOfWeekTest(item.input, item.isoStartDay, item.output);
+      const result = DateFns.getStartOfWeek(
+        moment(item.input),
+        item.isoStartDay
+      );
+      expect(result.format("YYYY-MM-DD")).toBe(item.output);
     });
   });
 });
