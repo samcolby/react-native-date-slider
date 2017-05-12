ad304a16e1d44164c8329eaebdeb8ed5c4688796
Sam Colby <samcolby@users.noreply.github.com>
2017-05-12 23:38:56 +0100

    Tests for initMoment

---
+++ __tests__/DateFns.initMoment.js
@@ -0,0 +1,39 @@
+import moment from "moment";
+
+import DateFns from "../src/DateFns";
+
+const testData = [
+  {
+    input: "2017-05-10",
+    locale: undefined,
+    output: "2017-05-09T23:00:00.000Z"
+  },
+  {
+    input: "2017-05-10",
+    locale: { name: "fr" },
+    output: "2017-05-09T23:00:00.000Z"
+  },
+  {
+    input: "2017-05-08",
+    locale: { name: "uk" },
+    output: "2017-05-07T23:00:00.000Z"
+  },
+  {
+    input: "2017-05-11",
+    locale: { name: "br" },
+    output: "2017-05-10T23:00:00.000Z"
+  }
+];
+
+describe("DateFns initMoment", () => {
+  testData.forEach(item => {
+    it(`should return ${item.output} when given ${item.input} and locale is ${JSON.stringify(item.locale)}`, () => {
+      const locale = item.locale
+        ? item.locale.name ? item.locale.name : "en"
+        : "en";
+      const result = DateFns.initMoment(moment(item.input), item.locale);
+      expect(result.toISOString()).toBe(item.output);
+      expect(result.locale()).toBe(locale);
+    });
+  });
+});
