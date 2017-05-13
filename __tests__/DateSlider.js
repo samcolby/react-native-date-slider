2e85eb4ab935a778aa435bac929a44f4583fd7e7
Sam Colby <samcolby@users.noreply.github.com>
2017-05-13 19:15:50 +0100

    Shallow Render Tests for DateSlider

---
+++ __tests__/DateSlider.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+
+import moment from "moment";
+
+import DateSlider from "../src/DateSlider";
+
+describe("DateSlider Component", () => {
+  it("should shallow render without issues", () => {
+    const component = shallow(<DateSlider />);
+
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it("should render a DateSliderComponent", () => {
+    const isoStartDay = 4;
+    const onDateSelected = jest.fn();
+    const onWeekChanged = jest.fn();
+    const selectedDate = new moment();
+    const component = shallow(
+      <DateSlider
+        isoStartDay={isoStartDay}
+        onDateSelected={onDateSelected}
+        onWeekChanged={onWeekChanged}
+        selectedDate={selectedDate}
+      />
+    );
+
+    expect(component.name()).toBe("DateSliderComponent");
+  });
+});
