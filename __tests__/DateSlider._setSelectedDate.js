4c23aa507610cee668ce7dd9d4c008770618f128
Sam Colby <samcolby@users.noreply.github.com>
2017-05-14 00:32:34 +0100

    Some tests for DateSlider._setSelectedDate

--- __tests__/DateSlider._setSelectedDate.js
+++ __tests__/DateSlider._setSelectedDate.js
@@ -1,32 +1,75 @@
 import React from "react";
 
 import { shallow } from "enzyme";
-import toJson from "enzyme-to-json";
 
+import _ from "lodash";
 import moment from "moment";
 
 import DateFns from "../src/DateFns";
 import DateSlider from "../src/DateSlider";
 
-describe("DateSlider _setSelectedDate", () => {
-  it("should change the selectedDate", () => {
-    const isoStartDay = 4;
-    const onDateSelected = jest.fn();
-    const onWeekChanged = jest.fn();
-    const selectedDate = moment("2017-05-11");
-    const component = shallow(
-      <DateSlider
-        isoStartDay={isoStartDay}
-        onDateSelected={onDateSelected}
-        onWeekChanged={onWeekChanged}
-        selectedDate={selectedDate}
-      />
-    );
-
-    const newSelectedDate = DateFns.initMoment("2017-02-11");
-
-    component.instance()._setSelectedDate(newSelectedDate);
-
-    expect(component.name()).toBe("DateSliderComponent");
+const getSelectedDate = component => {
+  const [week] = component.props().arrDates.filter(item => {
+    return _.some(item.arrSelected);
   });
+
+  const isoDay = _.indexOf(week.arrSelected, true);
+  return week.date.clone().add(isoDay, "days");
+};
+
+const doTest = (startDate, newDate, isoStartDay = 1) => {
+  const onDateSelected = jest.fn();
+  const onWeekChanged = jest.fn();
+  const selectedDate = moment(startDate);
+
+  const component = shallow(
+    <DateSlider
+      isoStartDay={isoStartDay}
+      onDateSelected={onDateSelected}
+      onWeekChanged={onWeekChanged}
+      selectedDate={selectedDate}
+    />
+  );
+
+  expect(getSelectedDate(component).valueOf()).toBe(selectedDate.valueOf());
+
+  const newSelectedDate = DateFns.initMoment(newDate);
+  component.instance()._setSelectedDate(newSelectedDate);
+
+  expect(getSelectedDate(component).valueOf()).toBe(newSelectedDate.valueOf());
+};
+
+describe("DateSlider _setSelectedDate", () => {
+  it("should change the selectedDate to the next day", () => {
+    doTest("2017-05-11", "2017-05-12", 4);
+  });
+
+  it("should change the selectedDate within the same week", () => {
+    doTest("2017-05-11", "2017-05-14", 4);
+  });
+
+  it("should change the selectedDate to the following week", () => {
+    doTest("2017-05-11", "2017-05-18", 1);
+  });
+
+  it("should change the selectedDate to the previous week", () => {
+    doTest("2017-05-11", "2017-05-04", 1);
+  });
+
+  it("should change the selectedDate to the following month", () => {
+    doTest("2017-05-11", "2017-06-11", 1);
+  });
+
+  it("should change the selectedDate to the previous month", () => {
+    doTest("2017-05-11", "2017-04-11", 1);
+  });
+
+  // TODO
+  // it("should change the selectedDate to the following year", () => {
+  //   doTest("2017-05-11", "2018-05-11", 1);
+  // });
+
+  // it("should change the selectedDate to the previous year", () => {
+  //   doTest("2017-05-11", "2015-05-11", 1);
+  // });
 });
